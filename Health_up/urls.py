ef36856dfd160424a9dc34ae69c49ad92301f007
madhurijadhav14755-dot <madhurijadhav14755@gmail.com>
2026-07-09 19:40:31 +0530

    updated project
    Signed-off-by: madhurijadhav14755-dot <madhurijadhav14755@gmail.com>

--- Health_up/urls.py
+++ Health_up/urls.py
@@ -3,12 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path('about/', views.about, name='about'),
 
+    path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 
-    path('dashboard/', views.dashboard, name='dashboard'),
     path('prediction/', views.prediction, name='prediction'),
+    path('result/', views.result, name='result'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('contact/', views.contact, name='contact'),
+
+    path('medicine/', views.medicine_info, name='medicine'),
 ]
