74ecce31058dcef6a61a7a02293855edab458935
madhurijadhav14755-dot <madhurijadhav14755@gmail.com>
2026-07-09 10:46:27 +0530

    updated project

---
+++ urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+    path('prediction/', views.prediction, name='prediction'),
+    path('result/', views.result, name='result'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('contact/', views.contact, name='contact'),
+
+    path('medicine/', views.medicine_info, name='medicine'),
+]
